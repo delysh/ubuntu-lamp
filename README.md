628bb5103e023560fe899d218998cc280bdb48b8
Yuriy Lyutov <yuriy@medialab.co>
2020-04-10 15:18:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Simple LAMP Vagrant box
+
+Simple LAMP environment inside Vagrant box. Based on official `ubuntu/bionic64` box.
+
+## In the box:
+
+- Ubuntu 18.04
+- Apache 2.4
+- MySQL 5.7
+- PHP 7.3
+- Node.js 12.x (with NPM)
+
+### Additionally installed:
+
+- phpMyAdmin
+- Git
+- Composer
+- Yarn
+
+## How to set up:
+
+Assuming that VirtualBox (https://www.virtualbox.org/) and Vagrant (https://www.vagrantup.com/) are already installed on your computer.
+
+1. Clone or download + unzip repository 
+2. In your terminal go to the directory and type `vagrant up`
+3. Wait (you may be prompt for system admin password)
+4. Enjoy
+
+## How to use:
+
+After setup is finished, go to http://192.168.33.10/ in your browser. You should see the `phpinfo()` page.
+`./www/`(or `/vagrant/www/` inside the box) is your "DocumentRoot". I recommend to keep your projects in the `projects/` folder. And make symlinks from `www/` to `public_html` folder of your project. But it’s up to you.
+
+`phpMyAdmin` is accessible at http://192.168.33.10/phpmyadmin/ Username is 'root', password - '12345678'
+
+*tested on Mac OS X 10.12.6 (Sierra) with VirtualBox v6.0.14 and Vagrant 2.2.6
---
+++ bootstrap.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Use single quotes instead of double quotes to make it work with special-character passwords
+PASSWORD='12345678'
+HOMEDIRFOLDER='www'
+PROJECTFOLDER='projects'
+
+# create project folder
+if [ -d "/var/www/html/${HOMEDIRFOLDER}" ];
+then
+	echo "/var/www/html/${HOMEDIRFOLDER} exists."
+else
+	sudo mkdir "/var/www/html/${HOMEDIRFOLDER}"
+fi
+
+if [ -d "/var/www/html/${PROJECTFOLDER}" ];
+then
+	echo "/var/www/html/${PROJECTFOLDER} exists."
+else
+	sudo mkdir "/var/www/html/${PROJECTFOLDER}"
+fi
+
+echo "<?php phpinfo(); ?>" > /var/www/html/${HOMEDIRFOLDER}/index.php
+
+# install PPA 'ondrej/php'. The PPA is well known, and is relatively safe to use.
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+
+# update / upgrade
+sudo apt -y update
+sudo apt -y upgrade
+
+# install apache
+sudo apt install -y apache2
+
+# removing all old php dependencies (not needed on fresh setup).
+#sudo apt-get remove -y php*
+
+# installing php7.3 and php modules
+sudo apt-get install -y php7.3 php7.3-cli php7.3-common libapache2-mod-php7.3 php7.3-fpm php7.3-curl php7.3-gd php7.3-bz2 php7.3-json php7.3-tidy php7.3-mbstring php-redis php-memcached php7.3-sqlite3 php7.3-xml php7.3-zip php7.3-readline php7.3-intl php7.3-bcmath php7.3-xmlrpc php7.3-recode php7.3-imagick php7.3-mysql
+
+# install mysql and give password to installer
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
+
+sudo apt install -y mysql-server
+
+# install phpmyadmin and give password(s) to installer
+# for simplicity I'm using the same password for mysql and phpmyadmin
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo apt install -y phpmyadmin
+
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${HOMEDIRFOLDER}"
+    <Directory "/var/www/html/${HOMEDIRFOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+# enable mod_rewrite
+sudo a2enmod rewrite
+
+# restart apache
+sudo systemctl restart apache2
+# restart mysql
+sudo systemctl restart mysql
+
+# install git
+sudo apt install -y git
+
+# install Composer
+#sudo apt install -y composer
+curl -sS https://getcomposer.org/installer |php
+sudo mv composer.phar /usr/local/bin/composer
+
+# install NodeJS and NPM
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install yarn
+npm install -g yarn
